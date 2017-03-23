a61bc7ac4e3cdb3820925999ad2f1463f471662b
Vukasin Radic <vukasin.radic.21@gmail.com>
2017-03-23 13:13:28 +0100

    first

---
+++ src/main/java/com/strale_vule/doc_app/DocAppApplication.java
@@ -0,0 +1,12 @@
+package com.strale_vule.doc_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/strale_vule/doc_app/DocAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.strale_vule.doc_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
